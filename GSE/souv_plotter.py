d547dffe4c8f1e73e7c3d8689cf4284fde2dbbd3
Souvadra <souvadra01@gmail.com>
2021-08-26 02:29:38 -0700

    GSE trial

---
+++ GSE/souv_plotter.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+file_name = "mer_hist.histo"
+x_list = []
+y_list = []
+file1 = open(file_name) 
+
+with open(file_name) as file:
+	i = 0
+	for line in file:
+		line = line.split()
+		x_list.append(int(line[0]))
+		y_list.append(int(line[1]))
+		i += 1
+		if i > 200:
+			break		
+
+plt.plot(x_list,y_list)
+plt.savefig('e_coli_hist.jpg')
